f6604ad7b1bca9798d7d4521ad49659791e53324
Alex Emelyanov <holyketzer@gmail.com>
2014-04-10 00:24:18 +0400

    Add twitter-bootstrap support. Add category creating

--- spec/acceptance/categories_spec.rb
+++ spec/acceptance/categories_spec.rb
@@ -27,11 +27,37 @@ feature 'Admin can manage categories', %q{
     expect(page).to have_link 'Удалить', href: category_path(category)
   end
 
+  scenario 'Admin creates new category' do
+    new_category = build(:category)
+    visit new_category_path
+
+    expect(page).to have_content 'Новая категория'
+
+    fill_in 'Название', with: new_category.title
+    fill_in 'Описание', with: new_category.description
+    fill_in 'Приоритет', with: new_category.priority
+    expect { click_on 'Сохранить' }.to change(Category, :count).by(1)
+
+    expect_category_page Category.last
+    expect(page).to have_content 'Категория сохранена'
+  end
+
   def expect_to_have_category(category)
     within "#category-#{category.id}" do
       expect(page).to have_content category.title
       expect(page).to have_content category.description
       expect(page).to have_content category.priority
     end
+  end
+
+  def expect_category_page(category)
+    expect(current_path).to match(category_path(category))
+
+    expect(page).to have_content 'Категория'
+    expect(page).to have_content 'Название'
+    expect(page).to have_content 'Описание'
+    expect(page).to have_content 'Приоритет'
+
+    expect_to_have_category category
   end
 end
