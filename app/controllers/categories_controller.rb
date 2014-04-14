28cbd42d6599c37c627b3afd863c20b1fa096f6b
Alex Emelyanov <holyketzer@gmail.com>
2014-04-14 23:38:49 +0400

    Add category editing and removing

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,16 @@
 class CategoriesController < InheritedResources::Base
   respond_to :html
 
+  def index
+    @categories = Category.order(:priority)
+  end
+
   def create
     create!(notice: t('activerecord.successful.messages.category_saved'))
+  end
+
+  def update
+    update!(notice: t('activerecord.successful.messages.category_saved'))
   end
 
   private
--- spec/acceptance/categories_spec.rb
+++ spec/acceptance/categories_spec.rb
@@ -21,7 +21,7 @@ feature 'Admin can manage categories', %q{
       expect(page).to have_content 'Приоритет'
     end
 
-    expect_to_have_category(category)
+    expect_to_have_category(category, with_link: true)
 
     expect(page).to have_link 'Изменить', href: edit_category_path(category)
     expect(page).to have_link 'Удалить', href: category_path(category)
@@ -33,17 +33,44 @@ feature 'Admin can manage categories', %q{
 
     expect(page).to have_content 'Новая категория'
 
-    fill_in 'Название', with: new_category.title
-    fill_in 'Описание', with: new_category.description
-    fill_in 'Приоритет', with: new_category.priority
+    fill_fields_for new_category
     expect { click_on 'Сохранить' }.to change(Category, :count).by(1)
 
     expect_category_page Category.last
     expect(page).to have_content 'Категория сохранена'
   end
 
-  def expect_to_have_category(category)
+  scenario 'Admin updates existing category' do
+    new_category = build(:category)
+    visit edit_category_path(category)
+
+    expect(page).to have_content 'Редактирование категории'
+    expect(page).to have_field 'Название', with: category.title
+    expect(page).to have_field 'Описание', with: category.description
+    expect(page).to have_field 'Приоритет', with: category.priority
+
+    fill_fields_for new_category
+    expect { click_on 'Сохранить' }.to change(Category, :count).by(0)
+
+    expect_category_page Category.find(category.id)
+    expect(page).to have_content 'Категория сохранена'
+  end
+
+  scenario 'Admin deletes existing category' do
+    visit category_path(category)
+
+    expect(page).to have_content category.title
+    expect { click_on 'Удалить' }.to change(Category, :count).by(-1)
+
+    expect(current_path).to match(categories_path)
+    expect(page).to_not have_content category.title
+  end
+
+  private
+
+  def expect_to_have_category(category, params = {})
     within "#category-#{category.id}" do
+      expect(page).to have_link category.title, href: category_path(category) if params[:with_link]
       expect(page).to have_content category.title
       expect(page).to have_content category.description
       expect(page).to have_content category.priority
@@ -59,5 +86,11 @@ feature 'Admin can manage categories', %q{
     expect(page).to have_content 'Приоритет'
 
     expect_to_have_category category
+  end
+
+  def fill_fields_for(category)
+    fill_in 'Название', with: category.title
+    fill_in 'Описание', with: category.description
+    fill_in 'Приоритет', with: category.priority
   end
 end
