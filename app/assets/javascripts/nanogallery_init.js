ec4a39c4eaee5d04eb2bf4eec88976109d0ce691
Alex Emelyanov <holyketzer@gmail.com>
2014-05-19 23:30:40 +0400

    Add nanogallery viewer for images in article

---
+++ app/assets/javascripts/nanogallery_init.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $("#nanoGallery").nanoGallery({
+    thumbnailWidth: 200,
+    thumbnailHeight: 200,
+    colorScheme: 'light',
+    colorSchemeViewer: 'light',
+    thumbnailLabel: {
+      display: false
+    }
+  });
+});
